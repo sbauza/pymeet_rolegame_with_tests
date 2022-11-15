03146a1bfcca6a9066516aa6cf7d396f8365075a
René Ribaud <rribaud@redhat.com>
2022-11-15 11:41:48 +0100

    feat: Add external service
    - Add an external service with 3 routes:
      /     -->     give status of the service.
      /dice -->     get a dice score beetween 1 and 12.
      /monsters --> get a random monster
    
    - Start the service with run.sh

---
+++ external_service/monsters.py
@@ -0,0 +1,50 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+MONSTERS = [
+    {
+        "name": "pytestire",
+        "health": 200,
+        "strength": 2,
+        "icon": "🧛",
+    },
+    {
+        "name": "toxmentor",
+        "health": 500,
+        "strength": 3,
+        "icon": "👹",
+    },
+    {
+        "name": "unitroll",
+        "health": 300,
+        "strength": 2,
+        "icon": "🧌",
+    },
+    {
+        "name": "fixtughost",
+        "health": 150,
+        "strength": 1,
+        "icon": "👻",
+    },
+    {
+        "name": "great python",
+        "health": 100,
+        "strength": 1,
+        "icon": "🐍",
+    },
+    {
+        "name": "mocklin",
+        "health": 180,
+        "strength": 2,
+        "icon": "👺",
+    },
+]
