4626cc9df62f57e59eb82ef7f975746f5685c671
Sylvain Bauza <sbauza@redhat.com>
2022-11-17 23:42:13 +0100

    added UT for explaining autospec and preparing some other game UTs

--- tests/unittest/test_game.py
+++ tests/unittest/test_game.py
@@ -15,21 +15,51 @@
 # THE MEETUP.
 
 import unittest
+from unittest import mock
+
+from rolegame import game
+
+
+class TestGameSpotted(unittest.TestCase):
+
+    def setUp(self):
+        self.patcher_client = mock.patch('rolegame.client')
+        self.mock_client = self.patcher_client.start()
+
+    def tearDown(self):
+        self.patcher_client.stop()
+
+    def test_old_spotted_implementation(self):
+        # let's pretend the client was having an old API named 'get_card'
+        # and that the spotted property was using it, so you wrote this test.
+        with mock.patch.object(game, 'client') as mock_client_notspecced:
+            mock_client_notspecced.Client.get_card.return_value = 'pikachu'
+            # in the old implementation, you were verifying that pikachu wasn't
+            # spotting you.
+            self.assertFalse(game.Game('1995_player').spotted)
+        # Now, the problem is that the test is still happy while it should raise
+        # some exception as we no longer use 'get_card' in 'spotted' method.
+
+        # Now, create a new mock that has a Client spec and do the same test.
+        with mock.patch.object(game, 'client', autospec=True) as mock_client:
+            try:
+                mock_client.Client.get_card.return_value = 'pikachu'
+                self.assertFalse(game.Game('1995_player').spotted)
+            except AttributeError:
+                # Now the test is failing, which is normal as we removed the
+                # call
+                pass
+        # You can see the difference between the mocks
+        print(mock_client_notspecced.Client)
+        print(mock_client.Client)
+
 
 class TestGame(unittest.TestCase):
 
     def setUp(self):
-        pass
-
-    def test_d(self):
-        pass
-
-class ExtraTestGame(TestGame):
-
-    def setUp(self):
-        return super().setUp()
+        self.patcher_client = mock.patch('rolegame.client', autospec=True)
+        self.mock_client = self.patcher_client.start()
+        self.addCleanup(self.patcher_client.stop)
 
     def test_e(self):
         pass
-
-# mocking de classe client avec setUp()
